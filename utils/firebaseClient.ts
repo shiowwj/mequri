0ede301cbf052d0e60d1954bdc0b0bfe03cf6592
shiowwj <shiowweijun@gmail.com>
2021-04-15 19:14:43 +0800

    remove unused firebase

--- utils/firebaseClient.ts
+++ utils/firebaseClient.ts
@@ -3,12 +3,6 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-const CURRENT_BUILD = process.env.NEXT_PUBLIC_ENV;
-const CURRENT_PERSISTENCE =
-  CURRENT_BUILD === 'PRODUCTION'
-    ? firebase.auth.Auth.Persistence.SESSION
-    : firebase.auth.Auth.Persistence.NONE;
-
 export const FIREBASE_CONFIG = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -28,19 +22,6 @@ if (!firebase.apps.length) {
   firebase.initializeApp(FIREBASE_CONFIG);
 }
 
-const app = firebase.app();
-const auth = firebase.auth();
 const db = firebase.firestore();
-const now = firebase.firestore.Timestamp.now();
-const storage = firebase.storage();
-console.log(
-  app.name
-    ? 'Firebase Mode Activated!'
-    : 'Firebase not working :('
-);
 
-if (app.name) {
-  firebase.auth().setPersistence(CURRENT_PERSISTENCE);
-}
-
-export { auth, db, now, storage };
+export { db };
